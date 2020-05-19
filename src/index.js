26e87a1e83aa8cfd74fc46f43d81d918f995e016
k-gorod <58625453+k-gorod@users.noreply.github.com>
2020-05-19 12:32:31 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 module.exports = function reverse (n) {
     var n2 = n.toString().split("").reverse();
-    n="";
-    n2.forEach(e => {
-        n+=e;
-    });
-    return parseInt(n,10);
+    return parseInt(n2.join(""),10);
 }
